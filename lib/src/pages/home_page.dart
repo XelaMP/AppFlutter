c5655a7f7424105d8caf2521bedce925fa4c8dea
Alexander Matta Pisconti <xelamp@MacBook-Pro-de-Alexander.local>
2019-11-02 00:23:36 -0500

    primer commit

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,47 @@
+
+import 'package:flutter/material.dart';
+import 'package:peliculas/src/widgets/card_swiper_widget.dart';
+
+
+class HomePage extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:  Text('Peliculas en cine'),
+        backgroundColor: Colors.indigoAccent,
+        actions: <Widget>[
+          IconButton (icon: Icon(Icons.search),
+          onPressed: (){
+
+          },
+          )
+
+        ],
+      ),
+      body: Container (
+        child: Column(
+          children: <Widget>[
+            _swiperTarjetas()
+          ],
+        ),
+      ),
+      
+      /*SafeArea(
+        child: 
+        Text('HOla mundo!!!'),
+      )*/
+
+      
+      
+    );
+  }
+
+  Widget _swiperTarjetas() {
+    return  CardSwiper(
+      peliculas: [1,2,3,4,5],
+    );
+  }
+}
